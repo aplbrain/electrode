66b46c2f2b4692fb4896118bf626c4b9b9a82a09
Jordan Matelsky <j6k4m8@gmail.com>
2017-09-29 16:29:46 -0400

    Add modules

---
+++ electrode/brain/__init__.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+"""
+Brain module.
+
+The master file for the simulator.
+"""
+
+from typing import List
+import networkx as nx
+
+from .. import timing
+from ..neuron import Neuron
+
+
+class Brain:
+    """
+    Brains handle simulator-pools.
+
+    More details inline.
+    """
+
+    def __init__(self, **kwargs):
+        """
+        Create a new Brain object to control neurons.
+
+        Arguments:
+            time_resolution (int : 500): Number of ns between "frames"
+
+        """
+        # Set defaults.
+        # Time resolution is 0.5 millis
+        self.time_resolution = kwargs.get('time_resolution', timing.ms(0.5))
+        self._graph = nx.Graph()
+        self.loaded = False
+
+    def __getitem__(self, key):
+        """
+        Get a neuron or segment data.
+
+        Arguments:
+            key (str[2]): The index to look up in the network
+
+        Returns:
+            Segment
+
+        """
+        raise NotImplementedError()
+
+    def load(self, reduce: bool = True):
+        """
+        Simplify the graph and load it into the networkx graph.
+
+        Arguments:
+            reduce (bool : True): Whether to attempt to reduce degree=2 edges
+                to a simplified, more optimized graph.
+
+        Returns:
+            None
+
+        """
+        self.loaded = True
+        if reduce:
+            # TODO: Reduce network.
+            pass
+        # raise NotImplementedError()
+
+    def run(self) -> bool:
+        """
+        Begin running the simulation.
+
+        Return True on every step, unless an error is encountered.
+
+        Arguments:
+            None
+
+        Returns:
+            bool: True when successful, False otherwise.
+
+        """
+        raise NotImplementedError()
+
+    def add_neuron(self, neuron):
+        """
+        Add a new neuron to the network. Adds resultant connections.
+
+        Cannot be run after a call to .load().
+
+        Arguments:
+            neuron (electrode.Neuron): The neuron to add.
+
+        Returns:
+            None
+
+        """
+        if not isinstance(neuron, Neuron):
+            raise TypeError("Neuron must implement electrode.neuron.Neuron.")
+        if self.loaded:
+            raise RuntimeError(
+                "You cannot call `add_neuron` after a call to `load`."
+            )
+
+    def add_synapse(
+            self, synapse,
+            source: List[str],
+            sink: List[str]
+    ):
+        """
+        Add a new synapse to the network.
+
+        Arguments:
+            synapse (electrode.Synapse): The synapse to add
+            source (str[2]): The presynaptic segment
+            sink (str[2]): The postsynaptic segment
+
+        Returns:
+            None
+
+        """
+        raise NotImplementedError()
---
+++ electrode/neuron/__init__.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+"""
+Neuron classes (implementations of neuron).
+
+You can implement Neuron to generate your own simulation type.
+"""
+
+from abc import ABCMeta, abstractmethod
+import typing
+
+import networkx as nx
+import uuid
+
+
+class Neuron:
+    """
+    The base Neuron class.
+
+    Implement me!
+
+    """
+
+    @abstractmethod
+    def frame(self) -> bool:
+        """
+        Iterate a frame in the neuron space.
+
+        In a simplified network fabric simulation, this is not used.
+        """
+        pass
+
+    @abstractmethod
+    def __repr__(self) -> str:
+        """
+        Generate a string representation.
+
+        Should be enough to repro a neuron in simple cases.
+        """
+        pass
+
+
+class HodgkinHuxleyNeuron(Neuron):
+    """
+    Hodgkin-Huxley point-model neuron.
+
+    References:
+        - [https://en.wikipedia.org/wiki/Threshold_potential]
+        - [https://en.wikipedia.org/wiki/Hodgkin%E2%80%93Huxley_model]
+
+    """
+
+    def __init__(
+            self,
+            name: str = None,
+            resting_potential: float = -75,
+            threshold_potential: float = -53
+    ):
+        """
+        Create a new Hodgkin-Huxley model point-neuron.
+
+        Accepts as arguments:
+            resting_potential (int : -75): Resting potential of the cell
+            threshold_potential (int : -75): Threshold for activation
+
+        """
+        if name:
+            self.name = name
+        else:
+            self.name = uuid.uuid4()
+
+        self.resting_potential = resting_potential
+        self.threshold_potential = threshold_potential
+
+    def frame(self):
+        """
+        Iterate a frame in the neuron space.
+
+        In a simplified network fabric simulation, this is not used.
+        """
+        pass
+
+    def __repr__(self) -> str:
+        """
+        Generate a string representation.
+
+        Simple enough to repro a neuron in simple cases.
+        """
+        return (
+            (
+                "HodgkinHuxley(name='{}', " +
+                "resting_potential={}, " +
+                "threshold_potential={})"
+            ).format(
+                self.name,
+                self.resting_potential,
+                self.threshold_potential
+            )
+        )
+
+    def reduce(self) -> nx.Graph:
+        """
+        Reduce the neuron to a graph representation.
+
+        The simple point-model neurons all reduce to a single node, "soma".
+
+        Arguments:
+            None
+
+        Returns:
+            networkx.Graph
+
+        """
+        g = nx.Graph()
+        g.add_node("{}/soma".format(self.name))
+        return g
