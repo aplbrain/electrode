fa2dd0076ac6df11ac22020138067e458c717cb2
McDaniel <sean.mcdaniel@jhuapl.edu>
2020-09-29 14:12:26 -0400

    initial workings of LIF neuron in electrode format

--- electrode/neuron/__init__.py
+++ electrode/neuron/__init__.py
@@ -54,7 +54,7 @@ class Neuron:
         """
         pass
 
-
+    
 class HodgkinHuxleyNeuron(Neuron):
     """
     Hodgkin-Huxley point-model neuron.
@@ -140,3 +140,70 @@ class HodgkinHuxleyNeuron(Neuron):
             }
         )
         return g
+
+
+class LeakyIntegratedandFire(Neuron):
+    """
+    Leaky Integrated and Fire neuron.
+
+    References:
+        - [https://en.wikipedia.org/wiki/Threshold_potential]
+        - [https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5557947/pdf/41598_2017_Article_7418.pdf]
+
+
+    self.neuron_group.namespace['tau'] = 10*ms #membrane time constant
+        self.neuron_group.namespace['vr'] = -40*mV #reset potential
+        self.neuron_group.namespace['vth'] = 20*mV #threshold potential
+        self.neuron_group.namespace['vh'] = -69.5*mV #holding potential
+        self.neuron_group.namespace['Eleak'] = 0*mV
+        self.neuron_group.namespace['g_leak'] = 1*usiemens
+
+    eq: dv/dt = -(v-I/g)/tau
+
+    threshold='v > vth'
+    refractory='2*ms'
+    method='exact'
+    reset='v= vr'
+
+    """
+    
+    def __init__(
+            self,
+            name: str = None,
+            resting_potential: float = -75,
+            fire_potential: float = 40,
+            threshold_potential: float = -53,
+    ) -> None:
+
+        """
+        Create a new LIF neuron
+
+        Accepts as arguments:
+            resting_potential (int : -75): Resting potential of the cell
+            fire_potential (int : -75): AP_max of the cell
+            threshold_potential (int : -75): Threshold for activation
+
+        """
+        if name:
+            self.name = name
+        else:
+            self.name = str(uuid.uuid4())
+
+        self.resting_potential = resting_potential
+        self.fire_potential = fire_potential
+        self.threshold_potential = threshold_potential
+
+    
+    def Frame():
+        """
+        Iterate a frame in the neuron space.
+
+        In a simplified network fabric simulation, this is not used.
+        """
+        pass
+
+    def __repr__(self):
+        pass
+    
+    def reduce(self):
+        pass
